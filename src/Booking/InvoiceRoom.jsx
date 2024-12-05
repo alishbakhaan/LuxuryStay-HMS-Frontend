e9edf38ded469a7efd437d059b4255e4f039421f
Alishba Khan❤️ <alishbakhaan649@gmail.com>
2024-12-05 19:56:40 +0500

    Update InvoiceRoom.jsx

--- src/Booking/InvoiceRoom.jsx
+++ src/Booking/InvoiceRoom.jsx
@@ -71,57 +71,58 @@ const InvoiceRoom = () => {
 
 
                         {/* <!-- Card form --> */}
-                        
-                            <div className='py-6'>
-                                <h2 className="text-2xl font-bold">Hey {reservationDetails.reservation.guest.profile.name},</h2>
-                                <p className="text-sm font-extralight mt-2">
-                                Your room ({reservationDetails.reservation.room.roomName + ' - ' + reservationDetails.reservation.room.roomNumber}) has been successfully booked! Enjoy a luxurious stay at our hotel. Please bring this
-                                invoice receipt with you upon arrival for a smooth check-in experience. We look forward to welcoming you! 
-                                </p>
 
-                                <div className="border-t border-gray-200 pt-4 mt-4">
-                                    <div className="flex justify-between">
-                                        <div>
-                                            <p className="text-gray-500 text-sm">Payment No.</p>
-                                            <strong>{reservationDetails.billing._id}</strong>
-                                        </div>
-                                        <div className="text-right">
-                                            <p className="text-gray-500 text-sm">Payment Date</p>
-                                            <strong>{new Date(reservationDetails.billing.issuedAt).toLocaleDateString("en-GB", {
+                        <div className='py-6'>
+                            <h2 className="text-2xl font-bold">Hey {reservationDetails.reservation.guest.profile.name},</h2>
+                            <p className="text-sm font-extralight mt-2">
+                                Your room ({reservationDetails.reservation.room.roomName + ' - ' + reservationDetails.reservation.room.roomNumber}) has been successfully booked! Enjoy a luxurious stay at our hotel. Please bring this
+                                invoice receipt with you upon arrival for a smooth check-in experience. We look forward to welcoming you!
+                            </p>
+
+                            <div className="border-t border-gray-200 pt-4 mt-4">
+                                <div className="flex justify-between">
+                                    <div>
+                                        <p className="text-gray-500 text-sm">Payment No.</p>
+                                        <strong>{reservationDetails.billing._id}</strong>
+                                    </div>
+                                    <div className="text-right">
+                                        <p className="text-gray-500 text-sm">Payment Date</p>
+                                        <strong>{new Date(reservationDetails.billing.issuedAt).toLocaleDateString("en-GB", {
                                             day: "2-digit",
                                             month: "2-digit",
                                             year: "numeric",
                                         })}</strong>
-                                        </div>
                                     </div>
                                 </div>
+                            </div>
 
-                                <div className="border-t border-gray-200 mt-4 py-4">
-                                    <div className="flex justify-between">
-                                        <div>
-                                            <p className="text-gray-500 text-sm">Client</p>
-                                            <strong>{reservationDetails.reservation.guest.profile.name}</strong>
-                                            <p className="text-sm">
-                                                {/* 989 5th Avenue, New York, 55832 */}
-                                                <br />
-                                                <a href="mailto:john@email.com" className="text-[#CDB9FF]">
+                            <div className="border-t border-gray-200 mt-4 py-4">
+                                <div className="flex justify-between">
+                                    <div>
+                                        <p className="text-gray-500 text-sm">Client</p>
+                                        <strong>{reservationDetails.reservation.guest.profile.name}</strong>
+                                        <p className="text-sm">
+                                            <br />
+                                            <a href="mailto:john@email.com" className="text-[#CDB9FF]">
                                                 {reservationDetails.reservation.guest.profile.contact.email}
-                                                </a>
-                                            </p>
-                                        </div>
+                                            </a>
+                                        </p>
                                     </div>
                                 </div>
+                            </div>
 
-                                <table className="w-full border-b border-gray-200 mt-3">
-                                    <thead>
-                                        <tr className="text-left text-gray-500 text-sm uppercase">
-                                            <th className="px-0 py-2">Room Name</th>
-                                            <th className="px-0 py-2">Persons</th>
-                                            <th className="px-0 py-2 text-right">Amount</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
+                            <table className="w-full border-b border-gray-200 mt-3">
+                                <thead>
+                                    <tr className="text-left text-gray-500 text-sm uppercase">
+                                        <th className="px-0 py-2">Room Name</th>
+                                        <th className="px-0 py-2">Persons</th>
+                                        <th className="px-0 py-2">Check-in</th>
+                                        <th className="px-0 py-2">Check-out</th>
+                                        <th className="px-0 py-2 text-right">Amount</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
                                         <td className="px-0 py-2">{reservationDetails.reservation.room.roomName}</td>
                                         <td className="px-0 py-2">{reservationDetails.reservation.room.persons}</td>
                                         <td className="px-0 py-2">{new Date(reservationDetails.reservation.checkIn).toLocaleDateString("en-GB", {
@@ -135,43 +136,33 @@ const InvoiceRoom = () => {
                                             year: "numeric",
                                         })}</td>
                                         <td className="px-0 py-2 text-right">{reservationDetails.reservation.totalAmount}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                    </tr>
+                                </tbody>
+                            </table>
 
-                                <div className="mt-5">
-                                    <div className="flex justify-end mt-3">
-                                        <h5 className="mr-3 text-lg font-bold">Total:</h5>
-                                        <h5 className="text-lg font-bold text-cyan-500">{reservationDetails.reservation.totalAmount}</h5>
-                                    </div>
+                            <div className="mt-5">
+                                <div className="flex justify-end mt-3">
+                                    <h5 className="mr-3 text-lg font-bold">Total:</h5>
+                                    <h5 className="text-lg font-bold text-cyan-500">{reservationDetails.reservation.totalAmount}</h5>
                                 </div>
                             </div>
-                            {/* <!-- Form footer --> */}
-                            <div className="mt-6">
-                                <div className="mb-4">
-                                    <button
+                        </div>
+                        {/* <!-- Form footer --> */}
+                        <div className="mt-6">
+                            <div className="mb-4">
+                                <button
                                     className="btn font-medium text-sm inline-flex items-center justify-center px-3 py-2 rounded-xl leading-5 shadow-sm transition duration-150 ease-in-out w-full">
-                                        <FontAwesomeIcon icon={faDownload} className='mr-2' />
-                                        Download
-                                    </button>
-                                </div>
-                                <div className="text-xs text-gray-500 italic text-center">Cash payment will be accepted upon arrival.</div>
+                                    <FontAwesomeIcon icon={faDownload} className='mr-2' />
+                                    Download
+                                </button>
                             </div>
+                            <div className="text-xs text-gray-500 italic text-center">Cash payment will be accepted upon arrival.</div>
+                        </div>
                     </div>
                 </div>
-
-
             </section>
         </>
     )
 }
 
 export default InvoiceRoom
-
-
-{/* <p>{reservationDetails.room.roomName}</p>
-<p>{reservationDetails.guest.profile.name}</p>
-<p>{reservationDetails.guest.profile.contact.email}</p>
-<p>{reservationDetails.room.roomNumber}</p>
-<p>{reservationDetails.room.persons}</p>
-<p>{reservationDetails.totalAmount}</p> */}
